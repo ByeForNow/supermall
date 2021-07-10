677328bc0e044466b035b8d09686f0a5a794ff9c
wangpeng <bfn@foxmai.com>
2021-07-10 10:40:46 +0800

    11

---
+++ src/components/common/navbar/NavBar.vue
@@ -0,0 +1,68 @@
+<!--  -->
+<template>
+    <div class="navbar">
+        <div class="left"><slot name="left"></slot></div>
+        <div class="center"><slot name="center"></slot></div>
+        <div class="right"><slot name="right"></slot></div>
+    </div>
+</template>
+
+<script>
+//导入组件、js、第三方插件、json文件、图片文件等等
+
+
+export default {
+    name:'NavBar    ',
+    //在components中注册组件
+    components: {
+    
+    },
+    //data里存放数据
+    data() {
+        return {
+        
+        };
+    },
+    //方法集合
+    methods: {
+    
+    },
+    //生命周期 - 创建完成（可以访问当前this实例）
+    created() {
+    
+    },
+    //生命周期 - 挂载完成（可以访问DOM元素）
+    mounted() {
+    
+    },
+    //监听属性 类似于data概念
+    computed: {},
+    //监控data中的数据变化
+    watch: {},
+    beforeCreate() {}, //生命周期 - 创建之前
+    beforeMount() {}, //生命周期 - 挂载之前
+    beforeUpdate() {}, //生命周期 - 更新之前
+    updated() {}, //生命周期 - 更新之后
+    beforeDestroy() {}, //生命周期 - 销毁之前
+    destroyed() {}, //生命周期 - 销毁完成
+    activated() {}, //如果页面有keep-alive缓存功能，这个函数会触发
+}
+</script>
+
+<style scoped>
+  .nav-bar {
+    display: flex;
+    height: 44px;
+    line-height: 44px;
+    text-align: center;
+    box-shadow: 0 1px 1px rgba(100,100,100,.1);
+  }
+
+  .left, .right {
+    width: 60px;
+  }
+
+  .center {
+    flex: 1;
+  }
+</style>
